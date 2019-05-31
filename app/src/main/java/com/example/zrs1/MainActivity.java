702797a2e28d3f7649994981d386f8e3b3e9e1c3
mhidoart <mhidoart@gmail.com>
2019-05-31 22:39:21 +0100

    initial commit

---
+++ app/src/main/java/com/example/zrs1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.zrs1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.btn1);
+
+
+    }
+}
